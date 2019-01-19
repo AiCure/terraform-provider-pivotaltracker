b4261692876715729def03723b91b46d2f9cf053
John Calabrese <xchapter7x@gmail.com>
2019-01-19 12:12:26 -0500

    start defining behavior of provider

--- pkg/trackerprovider/resources/projects/project_test.go
+++ pkg/trackerprovider/resources/projects/project_test.go
@@ -12,18 +12,55 @@ func TestProject(t *testing.T) {
 	RegisterTestingT(t)
 	t.Run("Create", func(t *testing.T) {
 		t.Skip("this is not yet implemented")
+		t.Run("when create fails", func(t *testing.T) {
+			err := projects.Create(nil, &ptfakes.FakeClientCaller{})
+			Expect(err).To(HaveOccurred(), "it should error")
+		})
+
+		t.Run("when it creates a new project", func(t *testing.T) {
+			err := projects.Create(nil, &ptfakes.FakeClientCaller{})
+			Expect(err).NotTo(HaveOccurred(), "it should not error")
+			Expect(nil).NotTo(BeNil(), "it should call the tracker api")
+			Expect(nil).NotTo(BeNil(), "it should set the schema to the value from tracker")
+		})
 	})
 
 	t.Run("Read", func(t *testing.T) {
 		t.Skip("this is not yet implemented")
+		t.Run("when read fails", func(t *testing.T) {
+			err := projects.Create(nil, &ptfakes.FakeClientCaller{})
+			Expect(err).To(HaveOccurred(), "it should error")
+		})
+
+		t.Run("when it reads an existing project", func(t *testing.T) {
+			err := projects.Read(nil, &ptfakes.FakeClientCaller{})
+			Expect(err).NotTo(HaveOccurred(), "it should not error")
+			Expect(nil).NotTo(BeNil(), "it should call the tracker api")
+			Expect(nil).NotTo(BeNil(), "it should set the schema to the value from tracker")
+		})
 	})
 
 	t.Run("Delete", func(t *testing.T) {
 		t.Skip("this is not yet implemented")
+		t.Run("when delete fails", func(t *testing.T) {
+			err := projects.Create(nil, &ptfakes.FakeClientCaller{})
+			Expect(err).To(HaveOccurred(), "it should error")
+		})
+
+		t.Run("when it deletes an existing project", func(t *testing.T) {
+			err := projects.Delete(nil, &ptfakes.FakeClientCaller{})
+			Expect(err).NotTo(HaveOccurred(), "it should not error")
+			Expect(nil).NotTo(BeNil(), "it should call delete on the project in the tracker api")
+		})
 	})
 
 	t.Run("Exists", func(t *testing.T) {
 		t.Skip("this is not yet implemented")
+		t.Run("when exists fails", func(t *testing.T) {
+			err := projects.Create(nil, &ptfakes.FakeClientCaller{})
+			Expect(err).To(HaveOccurred(), "it should error")
+		})
+
 		t.Run("when project doesnt exist", func(t *testing.T) {
 			exists, err := projects.Exists(nil, &ptfakes.FakeClientCaller{})
 			Expect(err).NotTo(HaveOccurred(), "it should not error")
@@ -39,5 +76,10 @@ func TestProject(t *testing.T) {
 
 	t.Run("Update", func(t *testing.T) {
 		t.Skip("this is not yet implemented")
+		t.Run("when update fails", func(t *testing.T) {
+			err := projects.Create(nil, &ptfakes.FakeClientCaller{})
+			Expect(err).To(HaveOccurred(), "it should error")
+		})
+
 	})
 }
